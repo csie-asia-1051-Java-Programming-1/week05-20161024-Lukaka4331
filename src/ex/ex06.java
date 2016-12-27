f7b99311ad7d58118a09878ef15373dd618b1236
Lukaka4331 <105021044@live.asia.edu.tw>
2016-12-27 20:07:24 +0800

    lll

--- src/ex/ex06.java
+++ src/ex/ex06.java
@@ -1,4 +1,4 @@
-﻿package ex;
+﻿package ex;import java.util.Scanner;
 /*
  * Topic: 某個陣列中含有所有學生的學號，但並沒有按照大小次序排列。請寫一程式依學號大小排列並將結果輸出。
  * Date: 2016/10/24
@@ -9,6 +9,30 @@ public class ex06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+          Scanner scn=new Scanner(System.in);
+          int n=scn.nextInt();
+          int data[]=new int [n];
+          int data1[]=new int [n];
+          int sum=0;
+          int c=-1;
+          for(int i=0;i<n;i++){
+        	  data[i]=scn.nextInt();
+          }
+          for(int i=0;i<n;i++){
+        	  for(int j=0;j<n;j++){
+        		while(data[j]>sum){
+        			sum=data[j];
+        			c=j;
+        		}
+        	}
+        	data1[i]=sum;
+        	data[c]=-1;
+        	sum=0;
+        	c=0;
+          }
+          for(int i=0;i<n;i++){
+        	  System.out.print(data1[i]+" ");
+          }
 
 	}
 
